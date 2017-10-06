276031d993f3915ac41a162939bb553ad4de47bb
IsaacShrestha <ishrestha@unomaha.edu>
2017-10-06 15:11:07 -0700

    duplicate errors fixed

--- app/components/kids-cancelride.js
+++ app/components/kids-cancelride.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	todayDate: null,
-
+	
 	todayDate: Ember.computed(function(){
 		var today = new Date();
 		return today;
--- app/components/kids-page.js
+++ app/components/kids-page.js
@@ -1,13 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	pageNameinUrl: null,
-
-	pageNameinUrl: Ember.computed(function(){
-  		var url = window.location.pathname;
-  		var pageNameinUrl = url.substring(url.lastIndexOf('/')+1);
-  		return pageNameinUrl;
-  	}),
 
 
 });

59bc224c9fc1fa8d3ece9a212a158ea03def3d3a
Abhishek Kumar <abhishekkumar@unomaha.edu>
2017-10-18 17:30:13 -0500

    register transition

--- app/routes/register.js
+++ app/routes/register.js
@@ -8,16 +8,23 @@ export default Ember.Route.extend({
             let controller = this.get('controller');
             let email = controller.get('email');
             let password = controller.get('password');
-            let role = controller.get('value');
+            let role = 'kids';
             let ref = this.get('firebaseApp').auth();
-            alert(role);
             ref.createUserWithEmailAndPassword(email, password).then(function(user){
-                this.replaceWith('login');
-                alert('lol');
-              }, function(error){
+                
+                this.get('session').open('firebase', {
+                    provider: 'password',
+                    email: email,
+                    password: password
+                  }).then(function(){                
+                      this.transitionTo(role+'.newprofile');
+                    }.bind(this))
+              }.bind(this), function(error){
                 alert(error);
-            }).bind(this);
-            
+            }.bind(this));          
+        },
+        selectRole(value){
+            this.set('currentValue', value);
         }
     }
 });

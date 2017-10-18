5ca30ef6b75c91167a11da140355b93a7742fa87
IsaacShrestha <ishrestha@unomaha.edu>
2017-10-18 13:07:59 -0700

    kids-ride-alert done

--- app/components/kids-ride-request.js
+++ app/components/kids-ride-request.js
@@ -3,6 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	startDate: '',
 
+	actions:{
+		sendReq(){
+			let alert = '';
+			this.set('alert', 'Request successfully send');
+		}
+	},
+
 	startDate: Ember.computed(function(){
 		let reqDate = new Date();
 		return reqDate;

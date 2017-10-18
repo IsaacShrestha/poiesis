f87a58ba40ad92ed7f823ad278dfc95617708c29
IsaacShrestha <ishrestha@unomaha.edu>
2017-10-18 10:22:22 -0700

    deleted files

--- app/router.js
+++ app/router.js
@@ -7,18 +7,26 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('login', {path: '/'});
-  this.route('login');
-  this.route('register');
-  this.route('kidsriderequests');
-  this.route('kidsridehistories');
-  this.route('kidsridecancels');
-  this.route('guardians');
-  this.route('guardianrequests');
-  this.route('guardiansridehistories');
-  this.route('guardiansridecancels');
-  this.route('landingPage');
-  this.route('dashboard');
+  
+  this.route('kids', function() {
+    this.route('newprofile');
+    this.route('newridereq', {path: '/'});
+    /*this.route('edit', {
+      path: ':todo_id/edit'
+    });*/
+
+
+    this.route('showprofile');
+
+    this.route('ridehistory');
+
+    this.route('showridereq');
+
+  });
+  
 });
 
+
 export default Router;
+
+
